f7ca49ff0f8e6e3c01b989ede49b61bf2cb91542
Shitij Thakur <shitij.thakur@expleogroup.com>
2024-03-08 17:19:10 +0530

    .

--- backend/chatbot/constants.py
+++ backend/chatbot/constants.py
@@ -1,3 +1,3 @@
-folders = {"folder_name": ["Lademanagement_VR04.2_11kW_OBC","LM_VR04.1_11kW_OBC"]}
+folders = {"folder_name": ["LM_VR04.2_11kW_OBC","Lademanagement_VR04.1_11kW_OBC"]}
 models = {"models": ["mistral-7b","mistral-7b-instruct"]}
 prompts =[{'title': 'Total Error Density', 'prompt':'What is the total error density?'},{'title': 'Information about test implementation increase?', 'prompt':'What are the test_implementation values?'}]
